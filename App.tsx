486a4a1d84a053239785a42ca99901d6ceb8a29f
Konstantin kokadevops <kokadevops@gmail.com>
2023-07-26 14:55:12 +0300

    add TabNavigator

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {},
+});
